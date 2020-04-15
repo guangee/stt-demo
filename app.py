b5117207f56bec034b45c465285e2ffad3c87aae
zziaguan <zziaguan@163.com>
2020-04-15 23:02:20 +0800

    快速提交:

--- app.py
+++ app.py
@@ -8,32 +8,33 @@ app = Flask(__name__)
 
 
 def get_wav_mfcc(wav_path):
-    f = wave.open(wav_path,'rb')
+    f = wave.open(wav_path, 'rb')
     params = f.getparams()
     # print("params:",params)
     nchannels, sampwidth, framerate, nframes = params[:4]
-    strData = f.readframes(nframes)#读取音频，字符串格式
-    waveData = np.fromstring(strData,dtype=np.int16)#将字符串转化为int
-    waveData = waveData*1.0/(max(abs(waveData)))#wave幅值归一化
-    waveData = np.reshape(waveData,[nframes,nchannels]).T
+    strData = f.readframes(nframes)  # 读取音频，字符串格式
+    waveData = np.fromstring(strData, dtype=np.int16)  # 将字符串转化为int
+    waveData = waveData * 1.0 / (max(abs(waveData)))  # wave幅值归一化
+    waveData = np.reshape(waveData, [nframes, nchannels]).T
     f.close()
 
     ### 对音频数据进行长度大小的切割，保证每一个的长度都是一样的【因为训练文件全部是1秒钟长度，16000帧的，所以这里需要把每个语音文件的长度处理成一样的】
     data = list(np.array(waveData[0]))
     # print(len(data))
-    while len(data)>16000:
-        del data[len(waveData[0])-1]
+    while len(data) > 16000 * 6:
+        del data[len(data) - 1]
         del data[0]
     # print(len(data))
-    while len(data)<16000:
+    while len(data) < 16000 * 6:
         data.append(0)
     # print(len(data))
 
-    data=np.array(data)
+    data = np.array(data)
     # 平方之后，开平方，取正数，值的范围在  0-1  之间
     data = data ** 2
     data = data ** 0.5
     return data
+
 
 @app.route('/')
 def video():
@@ -58,7 +59,7 @@ def video():
         if result[i] > result[ind]:
             ind = i
     print("识别的语音结果是：", name[ind])
-    t['result']=name[ind]
+    t['result'] = name[ind]
     return jsonify(t)
 
 
